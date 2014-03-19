0ea91eb297315ac73b26a2e324ea7bc6f5a3c773
Anders Lundholm <flaskhalz@gmail.com>
2014-03-19 19:37:21 +0100

    Added createsymlinks.sh to dotfiles

---
+++ createsymlinks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+dotfile_dir=~/.dotfiles
+bkup_dir=~/.dotfiles_bkup
+
+if [ ! -d "$bkup_dir" ]; then
+    echo -n "Creating $bkup_dir for backing up current dotfiles"
+    mkdir -p "$bkup_dir"
+fi  
+
+for file in ./*
+do
+    if [ -e ~/.${file##*/} ]; then
+        mv ~/.${file##*/} $bkup_dir
+        printf "Moving .${file##*/} to $bkup_dir\n"
+    fi 
+    ln -s ${file##*/} ~/\.${file##*/}
+    printf "\nCreating symlink from ${file##*/} to ~/.${file##*/}\n"
+done 
